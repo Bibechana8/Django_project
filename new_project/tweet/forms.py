3afdb464c3cbbb41ebb48bbb8f4fce3f86ef4fe8
Bibechana8 <bibechanapoudel25@gmail.com>
2026-07-19 22:07:28 +0545

    changes

--- new_project/tweet/forms.py
+++ new_project/tweet/forms.py
@@ -1,8 +1,17 @@
 from django import forms
 from .models import Tweet
+from django.contrib.auth.forms  import UserCreationForm
+from django.contrib.auth.models import User
 
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
         fields = ['text', 'photo']
-        
+
+class UserRegistrationForm(UserCreationForm):
+    username = forms.CharField(max_length=150)
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']  
